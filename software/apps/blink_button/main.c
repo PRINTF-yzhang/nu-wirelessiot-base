05c56c5c6796956f56b4c82a98b2a4a71433d376
Ying Zhang <31392353+PRINTF-yzhang@users.noreply.github.com>
2021-01-17 14:30:14 -0600

    test 1

--- software/apps/blink_button/main.c
+++ software/apps/blink_button/main.c
@@ -1,6 +1,6 @@
-// Buttons app
+// Button blink app
 //
-// Use buttons and switches to control LEDs
+// Use buttons and switches to control LEDs to blink
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -13,61 +13,48 @@
 
 int main(void) {
 
-  // Initialize.
-  nrf_gpio_cfg_output(LED1);
-  nrf_gpio_cfg_output(LED2);
-  nrf_gpio_cfg_output(LED3);
-  nrf_gpio_cfg_output(LED4);
-  nrf_gpio_pin_set(LED1);
-  nrf_gpio_pin_set(LED2);
-  nrf_gpio_pin_set(LED3);
-  nrf_gpio_pin_set(LED4);
-  nrf_gpio_cfg_input(BUTTON1, NRF_GPIO_PIN_PULLUP);
-  nrf_gpio_cfg_input(BUTTON2, NRF_GPIO_PIN_PULLUP);
-  nrf_gpio_cfg_input(BUTTON3, NRF_GPIO_PIN_PULLUP);
-  nrf_gpio_cfg_input(BUTTON4, NRF_GPIO_PIN_PULLUP);
+    // Initialize.
+    nrf_gpio_cfg_output(LED1);
+    nrf_gpio_cfg_output(LED2);
+    nrf_gpio_cfg_output(LED3);
+    nrf_gpio_cfg_output(LED4);
+    nrf_gpio_pin_set(LED1);
+    nrf_gpio_pin_set(LED2);
+    nrf_gpio_pin_set(LED3);
+    nrf_gpio_pin_set(LED4);
+    nrf_gpio_cfg_input(BUTTON1, NRF_GPIO_PIN_PULLUP);
+    nrf_gpio_cfg_input(BUTTON2, NRF_GPIO_PIN_PULLUP);
+    nrf_gpio_cfg_input(BUTTON3, NRF_GPIO_PIN_PULLUP);
+    nrf_gpio_cfg_input(BUTTON4, NRF_GPIO_PIN_PULLUP);
 
-  // Enter main loop.
-  while (1) {
-    if (nrf_gpio_pin_read(BUTTON1)) {
-      nrf_gpio_pin_set(LED1);
-        while (1) {
-            nrf_gpio_pin_toggle(LED1);
-            nrf_delay_ms(500);
-        }
-    } else {
-      nrf_gpio_pin_clear(LED1);
+    // Enter main loop.
+    while (nrf_gpio_pin_read(BUTTON2)) {
+        nrf_gpio_pin_set(LED2);
+        nrf_gpio_pin_toggle(LED2);
+        nrf_delay_ms(500);
+//        if (nrf_gpio_pin_read(BUTTON1)) {
+//            nrf_gpio_pin_set(LED1);
+//        } else {
+//            nrf_gpio_pin_clear(LED1);
+//        }
+//
+//        if (nrf_gpio_pin_read(BUTTON2)) {
+//            nrf_gpio_pin_set(LED2);
+//        } else {
+//            nrf_gpio_pin_clear(LED2);
+//        }
+//
+//        if (nrf_gpio_pin_read(BUTTON3)) {
+//            nrf_gpio_pin_set(LED3);
+//        } else {
+//            nrf_gpio_pin_clear(LED3);
+//        }
+//
+//        if (nrf_gpio_pin_read(BUTTON4)) {
+//            nrf_gpio_pin_set(LED4);
+//        } else {
+//            nrf_gpio_pin_clear(LED4);
+//        }
     }
-
-    if (nrf_gpio_pin_read(BUTTON2)) {
-      nrf_gpio_pin_set(LED2);
-        while (1) {
-            nrf_gpio_pin_toggle(LED2);
-            nrf_delay_ms(500);
-        }
-    } else {
-      nrf_gpio_pin_clear(LED2);
-    }
-
-    if (nrf_gpio_pin_read(BUTTON3)) {
-      nrf_gpio_pin_set(LED3);
-        while (1) {
-            nrf_gpio_pin_toggle(LED3);
-            nrf_delay_ms(500);
-        }
-    } else {
-      nrf_gpio_pin_clear(LED3);
-    }
-
-    if (nrf_gpio_pin_read(BUTTON4)) {
-      nrf_gpio_pin_set(LED4);
-        while (1) {
-            nrf_gpio_pin_toggle(LED4);
-            nrf_delay_ms(500);
-        }
-    } else {
-      nrf_gpio_pin_clear(LED4);
-    }
-  }
 }
 
