5e1954bd1e53890befabe66344532cb9cb732f90
Ying Zhang <31392353+PRINTF-yzhang@users.noreply.github.com>
2021-01-17 14:15:04 -0600

    new app

---
+++ software/apps/blink_button/main.c
@@ -0,0 +1,73 @@
+// Buttons app
+//
+// Use buttons and switches to control LEDs
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "nrf.h"
+#include "nrf_delay.h"
+#include "nrf_gpio.h"
+
+// Pin definitions
+#include "nrf52840dk.h"
+
+int main(void) {
+
+  // Initialize.
+  nrf_gpio_cfg_output(LED1);
+  nrf_gpio_cfg_output(LED2);
+  nrf_gpio_cfg_output(LED3);
+  nrf_gpio_cfg_output(LED4);
+  nrf_gpio_pin_set(LED1);
+  nrf_gpio_pin_set(LED2);
+  nrf_gpio_pin_set(LED3);
+  nrf_gpio_pin_set(LED4);
+  nrf_gpio_cfg_input(BUTTON1, NRF_GPIO_PIN_PULLUP);
+  nrf_gpio_cfg_input(BUTTON2, NRF_GPIO_PIN_PULLUP);
+  nrf_gpio_cfg_input(BUTTON3, NRF_GPIO_PIN_PULLUP);
+  nrf_gpio_cfg_input(BUTTON4, NRF_GPIO_PIN_PULLUP);
+
+  // Enter main loop.
+  while (1) {
+    if (nrf_gpio_pin_read(BUTTON1)) {
+      nrf_gpio_pin_set(LED1);
+        while (1) {
+            nrf_gpio_pin_toggle(LED1);
+            nrf_delay_ms(500);
+        }
+    } else {
+      nrf_gpio_pin_clear(LED1);
+    }
+
+    if (nrf_gpio_pin_read(BUTTON2)) {
+      nrf_gpio_pin_set(LED2);
+        while (1) {
+            nrf_gpio_pin_toggle(LED2);
+            nrf_delay_ms(500);
+        }
+    } else {
+      nrf_gpio_pin_clear(LED2);
+    }
+
+    if (nrf_gpio_pin_read(BUTTON3)) {
+      nrf_gpio_pin_set(LED3);
+        while (1) {
+            nrf_gpio_pin_toggle(LED3);
+            nrf_delay_ms(500);
+        }
+    } else {
+      nrf_gpio_pin_clear(LED3);
+    }
+
+    if (nrf_gpio_pin_read(BUTTON4)) {
+      nrf_gpio_pin_set(LED4);
+        while (1) {
+            nrf_gpio_pin_toggle(LED4);
+            nrf_delay_ms(500);
+        }
+    } else {
+      nrf_gpio_pin_clear(LED4);
+    }
+  }
+}
+
