90c71848fec6a01f37702f1d49b0646a8ed1c395
Ying Zhang <31392353+PRINTF-yzhang@users.noreply.github.com>
2021-01-28 20:44:24 -0600

    test1-step5

--- software/apps/ble_scan/main.c
+++ software/apps/ble_scan/main.c
@@ -21,36 +21,62 @@ static simple_ble_config_t ble_config = {
         .min_conn_interval = MSEC_TO_UNITS(500, UNIT_1_25_MS), // irrelevant if advertising only
         .max_conn_interval = MSEC_TO_UNITS(1000, UNIT_1_25_MS), // irrelevant if advertising only
 };
-simple_ble_app_t* simple_ble_app;
+simple_ble_app_t *simple_ble_app;
 
 
 // Callback handler for advertisement reception
-void ble_evt_adv_report(ble_evt_t const* p_ble_evt) {
+void ble_evt_adv_report(ble_evt_t const *p_ble_evt) {
 
-  // extract the fields we care about
-  ble_gap_evt_adv_report_t const* adv_report = &(p_ble_evt->evt.gap_evt.params.adv_report);
-  uint8_t const* ble_addr = adv_report->peer_addr.addr; // array of 6 bytes of the address
-  uint8_t* adv_buf = adv_report->data.p_data; // array of up to 31 bytes of advertisement payload data
-  uint16_t adv_len = adv_report->data.len; // length of advertisement payload data
+    // extract the fields we care about
+    ble_gap_evt_adv_report_t const *adv_report = &(p_ble_evt->evt.gap_evt.params.adv_report);
+    uint8_t const *ble_addr = adv_report->peer_addr.addr; // array of 6 bytes of the address
+    uint8_t *adv_buf = adv_report->data.p_data; // array of up to 31 bytes of advertisement payload data
+    /*
+     * 0x01	«Flags»
+     * 0x09	«Complete Local Name»
+     * 0x2B	«Mesh Beacon»
+     * 0x2D	«Broadcast_Code»
+     * 0xFF	«Manufacturer Specific Data»
+     */
+    uint16_t adv_len = adv_report->data.len; // length of advertisement payload data
 
-  printf("Received an advertisement!\n");
+    if (ble_addr[0] == 0xBB &&
+        ble_addr[1] == 0xAA &&
+        ble_addr[2] == 0x4E &&
+        ble_addr[3] == 0xE5 &&
+        ble_addr[4] == 0x98 &&
+        ble_addr[5] == 0xC0 &&) {
+        printf("Received an advertisement!\n");
+        printf("BLE address: ");
+        for (int i = 0; i <= 5; i++) {
+            printf("%x", ble_addr[i]);
+        }
+        printf("\nadvertisement payload data length: ");
+        printf("\nadvertisement payload data length: %d\n", adv_len);
+        for (int i = 0; i < adv_len; i++) {
+            printf("\n%x", adv_buf[i]);
+        }
+
+
+    }
+
 }
 
 
 int main(void) {
 
-  // Setup BLE
-  // Note: simple BLE is our own library. You can find it in `nrf5x-base/lib/simple_ble/`
-  simple_ble_app = simple_ble_init(&ble_config);
-  advertising_stop();
+    // Setup BLE
+    // Note: simple BLE is our own library. You can find it in `nrf5x-base/lib/simple_ble/`
+    simple_ble_app = simple_ble_init(&ble_config);
+    advertising_stop();
 
-  // Start scanning
-  scanning_start();
+    // Start scanning
+    scanning_start();
 
-  // go into low power mode
-  while(1) {
-    power_manage();
-  }
+    // go into low power mode
+    while (1) {
+        power_manage();
+    }
 }
 
 
